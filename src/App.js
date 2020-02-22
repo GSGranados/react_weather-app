7a482d3be4a7bd484c24473a9211f8820fa62113
GSGranados <stevengranados0211@gmail.com>
2020-02-21 18:09:58 -0600

    Folder Structure Created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Title from './Components/Title/Title';
+import Form from './Components/Form/Form';
+import Weather from './Components/Weather/Weather';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Title></Title>
+        <Form></Form>
+        <Weather></Weather>
+      </div>
+    );
+  }
 }
 
 export default App;
